fe124cfa4b52a2604ad6cd6b2edf7624af579881
PEACE-DFG <awofesobipeace@gmail.com>
2023-10-05 14:08:06 +0100

    validations

--- src/Component/Logi.jsx
+++ src/Component/Logi.jsx
@@ -1,12 +1,59 @@
 import './log.css'
 import Foot from '../Pages/foot';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 function Login() {
+  const navigate = useNavigate();
+
   const allStyles = {
     maxWidth: '400px',
     margin: 'auto',
 
   };
+  const [user,setUser]=useState({
+    email:'',
+    password:''
+  });
+  const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (localStorage.getItem('users')) {
+      const users = JSON.parse(localStorage.getItem('users'));
+      const matchedUser = users.find(
+        (u) => u.Email === user.email && u.Password === user.password
+      );
+      if (matchedUser) {
+        // Successful login
+        Swal.fire({
+          icon: 'success',
+          title: 'You are Logged In',
+        }).then(() => {
+          setTimeout(() => {
+            navigate('/');
+          }, 1000);
+        });
+      } else {
+       
+        setError('Invalid login details');
+      }
+    } else {
+      
+      setError('You have not registered. Please register first');
+    }
+  }
+  
+    
 
   return (
     <>
@@ -18,17 +65,20 @@ function Login() {
           <div className='text-center text-light' >
             <h2>Login</h2>
           </div>
-          <div className="form-floating mb-3">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label htmlFor="floatingInput">Email address</label>
+      <form action="" onSubmit={handleSubmit}>
+      <div className="form-floating mb-3">
+            <input type="email" className="form-control" name='email' id="floatingInput" value={user.email} onChange={handleChange} />
+            <label htmlFor="email">Email address</label>
           </div>
           <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label htmlFor="floatingPassword">Password</label>
+            <input type="password" className="form-control" id="floatingPassword" name='password' value={user.password} onChange={handleChange} />
+            <label htmlFor="password">Password</label>
           </div>
+          {error && <div className="alert alert-danger mt-3">{error}</div>}
           <div className="d-grid gap-2 col-12 mt-4 mx-auto">
-            <button className="btn btn-warning text-light" type="button"  style={{ fontWeight:'900' }}>Login</button>
+            <button className="btn btn-warning text-light" type="submit"  style={{ fontWeight:'900' }}>Login</button>
           </div>
+      </form>
         </div>
           <div className="particles" style={{ display:'flex' }}>
             {[...Array(28)].map((_, index) => (
@@ -36,6 +86,7 @@ function Login() {
             ))}
           </div>
         </div>
+      
       </section>
     </div>
     <Foot/>
@@ -43,4 +94,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Login
--- src/Component/Reg.jsx
+++ src/Component/Reg.jsx
@@ -1,12 +1,68 @@
 import './log.css'
 import Foot from '../Pages/foot';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+
 
 function Reg() {
     const allStyles = {
         maxWidth: '400px',
-        margin: 'auto',
-    
+        margin: 'auto'
+  
       };
+  const navigate = useNavigate();
+
+      const [user,setUser]=useState({
+        Name:'',
+        Email:'',
+        Password:'',
+        ConfirmPassword:''
+      });
+
+      const [errors,setErrors]=useState({});
+
+      const submitData = (e) => {
+        e.preventDefault();
+
+        const newErrors={};
+
+        if(user.Name.trim()===''){
+          newErrors.NameErr='Your Name is Required';
+        }
+        if(user.Email.trim()===''){
+          newErrors.EmailErr='Your Email is Required';
+        }
+        if(user.Password.trim()===''){
+          newErrors.PasswordErr='Your Password is Required';
+        }else if(user.Password.trim().length<8){
+          newErrors.PasswordErr='Your Password shouldnt be less than 8 characters';
+
+        }
+        if(user.ConfirmPassword.trim()===''){
+          newErrors.ConfirmPasswordErr='Your Password is Required';
+        }else if(user.Password.trim()!==user.ConfirmPassword.trim()){
+          newErrors.ConfirmPasswordErr='Your Password do not match';
+
+        }
+
+        setErrors(newErrors);
+        if (Object.keys(newErrors).length === 0) {
+          const users = JSON.parse(localStorage.getItem('users')) || [];
+          users.push(user);
+          localStorage.setItem('users', JSON.stringify(users));
+    
+          const name = user.Name;
+          Swal.fire({
+            icon: 'success',
+            title: 'Thank you for registering',
+            text: `${name}`,
+          }).then(() => {
+            navigate('/Login');
+          });
+        }
+
+      }
   return (
     <div>
          <div className='body'>
@@ -17,25 +73,49 @@ function Reg() {
           <div className='text-center text-light' >
             <h2>Register</h2>
           </div>
-          <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="floatingInput" placeholder="fullname" />
-            <label htmlFor="floatingInput">Fullname</label>
+     <form action="" onSubmit={submitData}>
+     <div className="form-floating mb-1">
+            <input type="text" className="form-control" id="floatingInput" placeholder="Name" 
+            onChange={(e)=>{
+              setUser({...user,Name:e.target.value})
+            }}
+            />
+            <label htmlFor="Name">Name</label>
+            {errors.NameErr && <span style={{ color: 'red',fontSize:'10px' }}>{errors.NameErr}</span>}
+
           </div>
-          <div className="form-floating mb-3">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label htmlFor="floatingInput">Email address</label>
+          <div className="form-floating mb-1">
+            <input type="email" className="form-control" id="floatingInput" placeholder="Email"
+              onChange={(e)=>{
+                setUser({...user,Email:e.target.value})
+              }} />
+            <label htmlFor="floatingInput">Email</label>
+            {errors.EmailErr && <span style={{ color: 'red',fontSize:'10px' }}>{errors.EmailErr}</span>}
+
           </div>
-          <div className="form-floating mb-3">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+          <div className="form-floating mb-1">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" 
+              onChange={(e)=>{
+                setUser({...user,Password:e.target.value})
+              }}
+            />
             <label htmlFor="floatingPassword">Password</label>
+            {errors.PasswordErr && <span style={{ color: 'red',fontSize:'10px' }}>{errors.PasswordErr}</span>}
+
           </div>
           <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <input type="password" className="form-control" id="floatingPassword" placeholder="ConfirmPassword"
+               onChange={(e)=>{
+                setUser({...user,ConfirmPassword:e.target.value})
+              }} />
             <label htmlFor="floatingPassword">ConfirmPassword</label>
+            {errors.ConfirmPasswordErr && <span style={{ color: 'red',fontSize:'10px' }}>{errors.ConfirmPasswordErr}</span>}
+
           </div>
-          <div className="d-grid gap-2 col-12 mt-4 mx-auto">
-            <button className="btn btn-warning text-light" type="button" style={{ fontWeight:'900' }}>Register</button>
+          <div className="d-grid gap-2 col-12 mt-3 mb-4 mx-auto">
+            <button className="btn btn-warning text-light" type="submit" style={{ fontWeight:'900' }}>Register</button>
           </div>
+     </form>
         </div>
           <div className="particles" style={{ display:'flex' }}>
             {[...Array(28)].map((_, index) => (
